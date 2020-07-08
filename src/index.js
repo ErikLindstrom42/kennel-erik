87acc96dda50d06bce73f165dcf53adef208e70a
Erik Lindstrom <elindstrom42@gmail.com>
2020-07-08 13:09:50 -0500

    application views complete

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Kennel from './components/Kennel';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Kennel from "./components/Kennel";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <Kennel />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import Kennel from './components/Kennel';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Kennel />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
