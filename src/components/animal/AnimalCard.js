504bd72aa3c11dfc153d074162e7c820b58977be
Erik Lindstrom <elindstrom42@gmail.com>
2020-07-09 16:05:35 -0500

    delete function for animals only

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,19 +1,23 @@
 import React from "react";
+import './Animal.css'
 
-const AnimalCard = (props) => {
+const AnimalCard = props => {
   return (
     <div className="card">
       <div className="card-content">
         <picture>
-          <img src={require("./dog.svg")} alt="My Dog" />
+          <img src={require('./dog.svg')} alt="My Dog" />
         </picture>
-        <h3>
-          Name: <span className="card-petname">{props.dog.name}</span>
-        </h3>
+        <h3>Name: <span className="card-petname">
+          {props.dog.name}
+        </span></h3>
         <p>Breed: {props.dog.breed}</p>
+        <button type="button" onClick={() => props.deleteAnimal(props.dog.id,"animals")}>Discharge</button>
       </div>
     </div>
   );
-};
+}
+
 
 export default AnimalCard;
+
--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Employee.css'
 
 const EmployeeCard = (props) => {
   return (
